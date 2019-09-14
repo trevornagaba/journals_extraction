01dd13a25cb86d2621d8e2f28d4d6478c0d580e7
Trevor Nagaba <trevornagaba@gmail.com>
2019-09-14 12:05:52 +0300

    Initial heavy commit

--- app/app.py
+++ app/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/index')
+def index():
+	context = { 'field_names':["GL_ACCCOUNT", "JOURNAL_ID", "DESCRIPTION", "TRANSACTION DATE", "VALUE DATE", "AUTO_MANUAL", 
+	"FSLI", "AMOUNT", "USER_ID"], 
+				'previous_names':["GL_ACCCOUNT", "JOURNAL_ID", "DESCRIPTION", "TRANSACTION DATE", "VALUE DATE", "AUTO_MANUAL", 
+	"FSLI", "AMOUNT", "USER_ID"],
+				'file_names':["Master", "Chart of Accounts", "Mapped TB", "User List"] 
+	}
+	return render_template('index.html', name=index, data=context)
